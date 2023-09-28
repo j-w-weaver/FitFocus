9275b2130201ec1fd772d6dfadd4dc70dba77224
John Weaver <john.weaver@prospectsplus.com>
2023-09-28 13:41:58 -0400

    added contact page

--- FitFocus/Controllers/HomeController.cs
+++ FitFocus/Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace FitFocus.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
